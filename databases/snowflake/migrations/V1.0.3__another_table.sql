fa9139958911841731879073ffe97823e1f36093
Akshay Karnawat <1731075+akshaykarnawat@users.noreply.github.com>
2023-03-22 16:25:08 -0400

    new change

---
+++ databases/snowflake/migrations/V1.0.3__another_table.sql
@@ -0,0 +1,19 @@
+
+-- Set the database and schema context
+USE SCHEMA {{ DB }}.{{ SCHEMA }};
+
+-- create a new test table
+CREATE OR REPLACE TABLE TEST_NEW 
+(
+     ID NUMBER
+    ,NOTES VARCHAR
+);
+
+-- insert some values into the table
+INSERT INTO TEST_NEW(ID, NOTES) 
+VALUES 
+(1, 'This is the first row') 
+,(2, 'This is the second row');
+
+
+ALTER TABLE TEST DROP COLUMN NOTES;
