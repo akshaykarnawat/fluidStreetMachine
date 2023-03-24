ed3c668842dce59430da7fa54e1f5b2216621da4
Akshay Karnawat <1731075+akshaykarnawat@users.noreply.github.com>
2023-03-24 00:16:17 -0400

    updated code

--- src/data/glue_jdbc_to_s3.py
+++ src/data/glue_jdbc_to_s3.py
@@ -1,4 +1,5 @@
 import sys
+
 # import boto3
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
@@ -6,7 +7,9 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME", "URL", "PORT", "USER", "PASSWORD", "QUERY", "DEST"])
+args = getResolvedOptions(
+    sys.argv, ["JOB_NAME", "URL", "PORT", "USER", "PASSWORD", "QUERY", "DEST"]
+)
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -19,20 +22,24 @@ job.init(args["JOB_NAME"], args)
 # connection_props = glue.get_connection(Name='database-1-connection', HidePassword=False).get('Connection', {}).get('ConnectionProperties', {})
 # glue.close()
 
-_ = (spark.read.format('jdbc')
-    .option('driver', 'com.mysql.cj.jdbc.Driver')
-    .option('url', args.get('URL'))                 #'jdbc:mysql://database-1.cgqybx2c0nza.us-east-1.rds.amazonaws.com'
-    .option('port', args.get('PORT'))               #'3306'
-    .option('user', args.get('USER'))               #'admin'
-    .option('password', args.get('PASSWORD'))       #'password123'
-    .option('dbtable', f"{args.get('QUERY')} AS t") #'(SELECT * FROM test.test_table LIMIT 10) AS t')
-    .option('fetchsize', '1000')
+_ = (
+    spark.read.format("jdbc")
+    .option("driver", "com.mysql.cj.jdbc.Driver")
+    .option(
+        "url", args.get("URL")
+    )  #'jdbc:mysql://database-1.cgqybx2c0nza.us-east-1.rds.amazonaws.com'
+    .option("port", args.get("PORT"))  #'3306'
+    .option("user", args.get("USER"))  #'admin'
+    .option("password", args.get("PASSWORD"))  #'password123'
+    .option(
+        "dbtable", f"{args.get('QUERY')} AS t"
+    )  #'(SELECT * FROM test.test_table LIMIT 10) AS t')
+    .option("fetchsize", "1000")
     .load()
     .coalesce(1)
-    .write
-    .format('parquet')
-    .mode('overwrite')
-    .save(args.get('DEST'))                         #'s3://test-122121/data'
+    .write.format("parquet")
+    .mode("overwrite")
+    .save(args.get("DEST"))  #'s3://test-122121/data'
 )
 
 job.commit()
