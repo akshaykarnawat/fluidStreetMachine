071902ef6ee69802e06e9bf711213f9053df927b
Akshay Karnawat <1731075+akshaykarnawat@users.noreply.github.com>
2023-04-06 17:34:41 -0400

    updating codebase

--- scripts/snowflake_lambda_pkg.sh
+++ scripts/snowflake_lambda_pkg.sh
@@ -47,8 +47,10 @@ function install_dependencies() {
 ###############################################################################
 function create_packaged_zip() {
     zip -r9 ~/snow_lambda.zip .
+    echo ${APP_PATH}
     pwd
-    zip -g ~/snow_lambda.zip ~/functions/run_snowflake_transformations.py
+    cd ~/workspace/
+    zip -g ~/snow_lambda.zip ./functions/run_snowflake_transformations.py
 }
 
 ###############################################################################
