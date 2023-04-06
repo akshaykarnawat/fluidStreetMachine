61d39cdcaf8f0b83b3cd53e87b2d7f567f560fa2
Akshay Karnawat <1731075+akshaykarnawat@users.noreply.github.com>
2023-04-06 17:55:29 -0400

    updating codebase

--- scripts/snowflake_lambda_pkg.sh
+++ scripts/snowflake_lambda_pkg.sh
@@ -7,28 +7,14 @@
 #               deployed as zip to lambda function.
 ###############################################################################
 
-# high level steps
-#virtualenv venv
-#source venv/bin/activate
-#cd venv/lib64/python3.9/site-packages/
-# https://github.com/pyca/cryptography/issues/6390
-# https://stackoverflow.com/questions/75389710/error-lib64-libc-so-6-version-glibc-2-28-not-found-required-by-var-task-c
-#pip3 install --platform manylinux_2_12_x86_64 --only-binary=:all: snowflake-connector-python --target .
-#chmod -R 755 .
-#zip -r9 ~/snow_lambda.zip .
-#cd ~
-#vim lambda_function.py
-# {{copy lambda function code in vim}}
-#zip -g snow_lambda.zip lambda_function.py
-#aws s3 cp snow_lambda.zip s3://snowflake-connector-lambda
 
 ###############################################################################
 # create_and_activate_virtual_env
 # Create and activate the virtual python environment
 ###############################################################################
 function create_and_activate_virtual_env() {
-    virtualenv snow_lambda
-    source snow_lambda/bin/activate
+    virtualenv ${ENV_NAME}
+    source ${ENV_NAME}/bin/activate
 }
 
 ###############################################################################
@@ -36,10 +22,10 @@ function create_and_activate_virtual_env() {
 # install dependencies in the virtual python environment
 ###############################################################################
 function install_dependencies() {
-    cd ./snow_lambda/lib*/python3.*/site-packages/
-    pip3 install --platform ${PLATFORM} --only-binary=:all: ${PACKAGE} \
-    --target .
-    chmod -R 755 .
+    local site_pkg_dir=./${ENV_NAME}/lib*/python3.*/site-packages/
+    cd ${site_pkg_dir}
+    pip3 install --platform ${PLATFORM} --only-binary=:all: ${PACKAGE} --target .
+    chmod -R 755 ${site_pkg_dir}
 }
 
 ###############################################################################
@@ -47,11 +33,10 @@ function install_dependencies() {
 # install dependencies in the virtual python environment
 ###############################################################################
 function create_packaged_zip() {
-    zip -r9 ~/workspace/snow_lambda.zip .
-    echo ${APP_PATH} ${SCRIPT_DIR_PATH}
-    pwd
-    cd ~/workspace/
-    zip -g ~/workspace/snow_lambda.zip ./functions/run_snowflake_transformations.py
+    local zip_file_location=~/workspace/${ENV_NAME}.zip
+    zip -r9 ${zip_file_location} .
+    # cd ~/workspace/
+    zip -g ${zip_file_location} ${LAMBDA_FUNC_LOCATION}/*
 }
 
 ###############################################################################
@@ -60,12 +45,15 @@ function create_packaged_zip() {
 USER=$(whoami)
 SCRIPT=$0
 SCRIPT_DIR_PATH="${0%/*}"
+ENV_NAME=${1}
+LAMBDA_FUNC_LOCATION=${2}
 
 source ${SCRIPT_DIR_PATH}/logger.sh
 
 APP_PATH=$(dirname ${SCRIPT_DIR_PATH})
 RUN_LOG_FILE=$(logger::init ${APP_PATH}/logs snowflake_lambda_pkg)
 CURRENT_TIME="$(date +'%Y%m%d%H%M%S')"
+
 
 PLATFORM=manylinux_2_12_x86_64
 PACKAGE=snowflake-connector-python
