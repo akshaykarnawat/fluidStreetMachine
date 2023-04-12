d8c1eef99a65401179188dd7b90b54e1bd93199c
Akshay Karnawat <1731075+akshaykarnawat@users.noreply.github.com>
2023-04-12 11:21:13 +0530

    updated code

--- scripts/deploy_aws.sh
+++ scripts/deploy_aws.sh
@@ -190,7 +190,6 @@ function deploy_orchestration() {
         --parameters \
             ParameterKey=ProjectName,ParameterValue=${PROJECT_NAME} \
             ParameterKey=Environment,ParameterValue=${ENVIRONMENT} \
-            # ParameterKey=ExecutionInput,ParameterValue=${execution_input} \
         --region ${AWS_REGION} --capabilities CAPABILITY_IAM
         sleep 30
     fi
