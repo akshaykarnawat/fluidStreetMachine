2e83401cc249ede451dc029d083325835de068eb
Akshay Karnawat <1731075+akshaykarnawat@users.noreply.github.com>
2023-04-12 10:50:27 +0530

    updated code

--- scripts/deploy_aws.sh
+++ scripts/deploy_aws.sh
@@ -161,9 +161,8 @@ function create_data_bucket() {
 function deploy_orchestration() {
     local stack_name=${1}
     local code_deploy_bucket=${2}
-    local execution_input=$(echo $(cat "${3}") | tr -d '[\n\t ]')
-    # execution_input=$(echo \"${execution_input//\"/\\\"}\")
-    execution_input=$(echo ${execution_input//\"/\\\"})
+    local execution_input=$(echo $(cat "${3}") | tr -d '[ ]')
+    execution_input=$(echo \"${execution_input//\"/\\\"}\")
 
     stack=$(describe_stacks "Stacks[?contains(StackName,'${stack_name}')].StackName" text)
     # if the stack does not exist create it, else update the stack's function
